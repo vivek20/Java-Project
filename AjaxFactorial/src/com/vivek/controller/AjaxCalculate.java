6565fde941b24e5c0d7aadd1b8085207f32ce4e2
vivek2563 <vivekbhatia560@gmail.com>
2018-04-22 12:08:38 +0530

    Commited by Vivek Bhatia Declared two new Variables only.

---
+++ AjaxFactorial/src/com/vivek/controller/AjaxCalculate.java
@@ -0,0 +1,53 @@
+package com.vivek.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class AjaxCalculate
+ */
+public class AjaxCalculate extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AjaxCalculate() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		
+		int i,j,k;
+		long f=1;
+		int number=Integer.parseInt(request.getParameter("number"));
+		PrintWriter out=response.getWriter();
+		
+	for(i=1;i<=number;i++)
+	{
+		f=f*i;
+	}
+	
+	out.write(""+f);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
